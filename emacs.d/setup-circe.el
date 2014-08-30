f179b77f2a301ffa18160218fb9ff6bd4f9614fe
Joram Schrijver <i@joram.io>
2014-08-30 14:14:59 +0200

    Properly set up Circe

--- emacs.d/setup-circe.el
+++ emacs.d/setup-circe.el
@@ -1,8 +1,38 @@
 
+(load "~/.private.el")
 (let ((circe-path
        (expand-file-name "site-lisp/circe/lisp" user-emacs-directory)))
   (add-to-list 'load-path circe-path))
 
 (require 'circe)
+(require 'circe-color-nicks)
+
+(setq circe-color-nicks-everywhere t)
+
+(add-to-list 'ido-ignore-buffers "#.*")
+
+(defun ido-switch-irc-buffer ()
+  (interactive)
+  (let ((ido-ignore-buffers '("^[^#]*$")))
+    (ido-switch-buffer)))
+
+(global-set-key (kbd "C-x I") 'ido-switch-irc-buffer)
+
+(setq circe-network-options
+      `(("zfn"
+         :nick "joram"
+         :host "irc.joram.io"
+         :service 2345
+         :pass (concat "joram/freenode:" ,znc-password))
+        ("zfn"
+         :nick "joram"
+         :host "irc.joram.io"
+         :service 2345
+         :pass (concat "joram/freign:" ,znc-password))
+        ("zim"
+         :nick "joram"
+         :host "irc.joram.io"
+         :service 2345
+         :pass (concat "joram/im:" ,znc-password))))
 
 (provide 'setup-circe)

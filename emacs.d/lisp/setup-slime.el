9d40e769021d204a2a9b95a54f8d1a57d53fab1d
Joram Schrijver <i@joram.io>
2015-06-19 23:21:06 +0200

    Emacs: Move the local Hyperspec
    To a more appropriate directory. I'm slowly moving my 'dev' directory to
    a different folder structure. Keeping the HyperSpec there doesn't make
    sense anymore.

--- emacs.d/lisp/setup-slime.el
+++ emacs.d/lisp/setup-slime.el
@@ -3,7 +3,7 @@
 (setq inferior-lisp-program "sbcl")
 (setq slime-repl-history-file
       (expand-file-name "backups/slime-history.eld" user-emacs-directory))
-(setq common-lisp-hyperspec-root "file:///home/joram/dev/lisp/HyperSpec/")
+(setq common-lisp-hyperspec-root "file:///home/joram/.dump/HyperSpec/")
 
 (slime-setup '(slime-fancy slime-company slime-indentation slime-asdf))
 

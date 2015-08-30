2d11b522e0a47f6e25d8ba6918df524bcb9028e8
Joram Schrijver <i@joram.io>
2015-08-30 16:22:09 +0200

    Emacs: Add mc/add-cursor-on-click hot-button

--- emacs.d/lisp/setup-non-evil.el
+++ emacs.d/lisp/setup-non-evil.el
@@ -3,6 +3,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this-dwim)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)

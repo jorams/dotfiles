69489646629107bf2c24484f9a820a5b1fa4a48a
Joram Schrijver <i@joram.io>
2015-11-30 19:44:46 +0100

    Autome: Add Linkbox option to read STDIN

--- autome/src/linkbox.lisp
+++ autome/src/linkbox.lisp
@@ -25,20 +25,34 @@
 (defun upload (path)
   (request "file" (parse-namestring path)))
 
-(defun upload/shorten (path/url)
+(defun upload-stdin (type)
+  (with-temporary-file (:stream stream
+                        :pathname pathname
+                        :element-type '(unsigned-byte 8)
+                        :direction :output
+                        :type (or type "tmp"))
+    (slurp-input-stream stream
+                        *standard-input*
+                        :element-type '(unsigned-byte 8))
+    (finish-output stream)
+    (upload pathname)))
+
+(defun upload/shorten (&optional path/url type)
   (match path/url
     ((and (or (ppcre "^mailto:")
               (ppcre "://"))
           url)
      (shorten url))
+    (nil (upload-stdin type))
     (file (upload file))))
 
 (define-entry cli
     (:description "Upload a file or create a short url."
      :free free
-     :commands (("<path/url>" "upload <path> or shorten <url>")))
+     :opts ((type #\t "File type, only relevant for STDIN" #'identity "TYPE"))
+     :commands (("[path/url/]" "upload <path>, shorten <url> or post stdin")))
   (multiple-value-bind (result status)
-      (apply 'upload/shorten free)
+      (upload/shorten (first free) type)
     (when (and result (= status 200))
       (prog1 result
         (princ result)

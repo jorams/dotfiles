5095b259522d8e98c795b110fecbc6a263a233cb
Joram Schrijver <i@joram.io>
2018-08-08 22:23:27 +0200

    Emacs: Switch theme to doom-one

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -265,7 +265,7 @@ point reaches the beginning or end of the buffer, stop there."
 
 (defun j/load-theme ()
   (set-frame-parameter nil 'background-mode 'dark)
-  (load-theme 'spacemacs-dark t)
+  (load-theme 'doom-one t)
   (set-frame-font (font-spec :family "Input Mono"
                              :size (if (getenv "IS4K") 22 11))
                   nil

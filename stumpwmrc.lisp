f0bf1e06441b70015113e45ebd611073d281f65c
Joram Schrijver <i@joram.io>
2019-01-02 22:33:48 +0100

    StumpWM: Replace i3lock binding with slock

--- stumpwmrc.lisp
+++ stumpwmrc.lisp
@@ -106,7 +106,8 @@
   ;; Theme, also not xrandr-related
   ("y" "theme home")
   ("Y" "theme work")
-  ("l" "exec bash -c 'i3lock -f -i <(import -window root - | convert -scale 20% -scale 500% - png:-)'"))
+  ;; Locking, similarly not xrandr-related
+  ("l" "exec bash -c slock"))
 
 (define-key *root-map* (kbd "d") '*xrandr-map*)
 

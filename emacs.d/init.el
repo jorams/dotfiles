a0e638a4f60b1d3db2a64840457975cd0317a3d9
Joram Schrijver <i@joram.io>
2023-04-24 08:28:19 +0200

    Add DOTFILE_FONT_RESOLUTION
    For screens with higher pixel density. It covers similar ground to IS4K,
    so I might merge them at some point.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -457,7 +457,9 @@ point reaches the beginning or end of the buffer, stop there."
   (setq doom-themes-treemacs-theme "doom-colors")
   (doom-themes-treemacs-config)
   (set-frame-font (font-spec :family "Input"
-                             :size (if (getenv "IS4K") 20 11))
+                             :size (cond ((getenv "IS4K") 20)
+                                         ((getenv "DOTFILE_FONT_RESOLUTION") 16)
+                                         (t 11)))
                   nil
                   t))
 

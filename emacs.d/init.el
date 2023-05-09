5171f797c99ae8bb8752901f29585d283d6f1ec4
Joram Schrijver <i@joram.io>
2023-05-10 01:51:19 +0200

    Emacs: Disable paredit in minibuffer eval

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -648,12 +648,11 @@ The value is not entered into the kill ring, but copied using
   :commands enable-paredit-mode
   :diminish "()"
   :init
-  (add-hook 'emacs-lisp-mode-hook                  'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook                        'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook                        'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook            'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook                      'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           'enable-paredit-mode)
   :config
   (setq paredit-space-for-delimiter-predicates
         '((lambda (endp delimiter) nil))))
